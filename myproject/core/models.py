b61de2d7ac9775da49913fa4e2437e19f11814e2
rg3915 <rg3915@yahoo.com.br>
2018-10-03 19:57:43 -0300

    First commit

---
+++ myproject/core/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class Person(models.Model):
+    name = models.CharField('nome', max_length=100)
+    email = models.EmailField(null=True, blank=True)
+    phone = models.CharField('telefone', max_length=11, null=True, blank=True)
+    GENDER = (
+        ('0', ''),
+        ('man', 'homem'),
+        ('woman', 'mulher'),
+    )
+    gender = models.CharField(
+        'sexo',
+        max_length=5,
+        choices=GENDER,
+        default='0'
+    )
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Contato'
+        verbose_name_plural = 'Contatos'
+
+    def __str__(self):
+        return self.name
---
+++ myproject/core/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import Person
+
+
+def index(request):
+    context = {}
+    person_list = Person.objects.all()
+    context['person_list'] = person_list
+    return render(request, 'index.html', context)
