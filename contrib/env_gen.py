81024cee3b776fda74bf6643c529199d75bf7d1f
Regis Santos <regis42santos@gmail.com>
2021-09-12 19:08:40 -0300

    Atualiza para Django 3.2.7

--- contrib/env_gen.py
+++ contrib/env_gen.py
@@ -1,26 +1,39 @@
-#!/usr/bin/env python
-
 """
-Django SECRET_KEY generator.
+Python SECRET_KEY generator.
 """
-from django.utils.crypto import get_random_string
+import random
 
-chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+chars = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!?@#$%^&*()"
+size = 50
+secret_key = "".join(random.sample(chars, size))
+
+chars = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!?@#$%_"
+size = 20
+password = "".join(random.sample(chars, size))
 
 CONFIG_STRING = """
 DEBUG=True
 SECRET_KEY=%s
-ALLOWED_HOSTS=127.0.0.1, .localhost
+ALLOWED_HOSTS=127.0.0.1,.localhost,0.0.0.0
+
 #DATABASE_URL=postgres://USER:PASSWORD@HOST:PORT/NAME
+#POSTGRES_DB=
+#POSTGRES_USER=
+#POSTGRES_PASSWORD=%s
+#DB_HOST=localhost
+
 #DEFAULT_FROM_EMAIL=
 #EMAIL_BACKEND=django.core.mail.backends.smtp.EmailBackend
-#EMAIL_HOST=
+#EMAIL_HOST=localhost
 #EMAIL_PORT=
-#EMAIL_USE_TLS=
 #EMAIL_HOST_USER=
 #EMAIL_HOST_PASSWORD=
-""".strip() % get_random_string(50, chars)
+#EMAIL_USE_TLS=True
+""".strip() % (secret_key, password)
 
 # Writing our configuration file to '.env'
 with open('.env', 'w') as configfile:
     configfile.write(CONFIG_STRING)
+
+print('Success!')
+print('Type: cat .env')
